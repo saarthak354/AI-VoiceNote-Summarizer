5493645f2f86ac5160a4dcd344c3c38e9ab6e78d
Saarthak Singh <saarthak354@gmail.com>
2026-02-06 21:31:37 +0530

    handling temp test cases

--- backend/test_intent.py
+++
@@ -1,8 +0,0 @@
-from intent import extract_intent
-
-text = """
-I really don’t like how unprepared I felt in today’s meeting.
-I should probably email the team lead tomorrow and ask for clearer expectations.
-"""
-
-print(extract_intent(text))
